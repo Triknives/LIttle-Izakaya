06d324ccea26aff83ba665d1ba81a855593175f9
Student <example@example.com>
2020-02-19 10:23:17 -0800

    update styling add footer file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const footerStyle = {
+  backgroundColor: 'white',
+  height:'20px'
+  
+function Footer(){
+
+  }
+  const links = {
+    color: 'white',
+  }
+  return (
+    <div style = {footerStyle}>
+
+    </div>
+  );
+}
+
+
+export default Footer;
