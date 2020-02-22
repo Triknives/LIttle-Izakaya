4a8a009c4fd6f4c002a5e63ec5aee0849959412d
Triknives <50305423+Triknives@users.noreply.github.com>
2020-02-21 17:19:53 -0800

    update footer placement, styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-const footerStyle = {
-  backgroundColor: 'white',
-  height:'25px',
-  width:'60%',
-  alignItems:'center',
-  textAlign:'center',
-  opacity:'.85',
-  margin: '1vh auto',
-}
-
 function Footer(){
   return (
-    <div style = {footerStyle}>
+    <div className ='footer'>
     <footer>&copy;2020 Design by Brett Cordell - Cordell.desu@gmail.com</footer>
     </div>
   );
